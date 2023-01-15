365e356c22f2a47a1f45de0f600260a5128c5ef5
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2023-01-15 20:52:08 +0200

    dependencies were added

---
+++ src/test/java/ua/masaltsev/chatsb/ChatSbApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.masaltsev.chatsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
